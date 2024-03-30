1292604b1f777dd423757b7d3992c037dd1d7220
Alvin <kyusyer@gmail,com>
2024-03-31 02:03:47 +0800

    added menu burger and javascript

--- services.js
+++ services.js
@@ -6,17 +6,16 @@ function cardSwitching() {
 
     console.log("start switch")
 
-    // setTimeout(() => {
-    //     console.log("card1")
+
 
     card1.style.display = "flex";
     card2.style.display = "none";
     card3.style.display = "none";
 
-    // }, 2000)
+
 
     setTimeout(() => {
-        console.log("card2")
+
 
         card1.style.display = "none";
         card2.style.display = "flex";
@@ -28,7 +27,7 @@ function cardSwitching() {
 
 
     setTimeout(() => {
-        console.log("card3")
+
 
         card1.style.display = "none";
         card2.style.display = "none";
@@ -44,10 +43,44 @@ function cardSwitching() {
 document.addEventListener('DOMContentLoaded', function () {
 
     setInterval(cardSwitching, 9000);
-}
+
+    window.onscroll = () => {
+        console.log("scrolling detected")
+        if (window.scrollY >= window.innerHeight) {
+
+
+            document.querySelector('.call').style.top = '0';
+
+        }
+        else {
+            document.querySelector('.call').style.top = '90vh';
+        }
+    }
+
+    document.querySelector("#nav-burger").onclick = function () {
+
+        let navBurger = document.querySelector("#nav-burger-menu")
+
+        if (navBurger.style.display === "none") {
+            navBurger.style.display = "flex";
+        }
+        else {
+            navBurger.style.display = "none";
+        }
+
+    }
+
+    window.addEventListener("resize", function () {
+        if (window.matchMedia("(min-width: 801px)").matches) {
+            console.log("Screen width is at least 500px");
+            document.querySelector("#nav-menu-burger").style.display = "none";
+        }
+
+    });
 
 
 
 
 
-)
+
+})
